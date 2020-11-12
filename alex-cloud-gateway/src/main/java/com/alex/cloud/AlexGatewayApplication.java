2bbac4d5f20556610e237255c371e583be8330b0
wubo <wubo@artigent.cn>
2020-11-12 16:32:09 +0800

    init

---
+++ alex-cloud-gateway/src/main/java/com/alex/cloud/AlexGatewayApplication.java
@@ -0,0 +1,12 @@
+package com.alex.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlexGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AlexGatewayApplication.class, args);
+    }
+
+}
